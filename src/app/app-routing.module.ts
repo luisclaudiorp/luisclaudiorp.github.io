6dfad0d015ad67aa17f914a97557f8762afaafde
luisclaudiorp <61363429+luisclaudiorp@users.noreply.github.com>
2023-04-28 16:03:55 -0300

    feat: add styles and structure

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+  path: '',
+  loadChildren: () => import('./core/components/auth/auth.module').then(m => m.AuthModule)
+},
+{
+  path: 'admin',
+  loadChildren: () => import('./components/admin/admin.module').then(m => m.AdminModule)
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
