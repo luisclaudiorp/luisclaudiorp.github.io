f3a0cc3f4b810c91b714c705ab2d62c55ef1cf91
luisclaudiorp <61363429+luisclaudiorp@users.noreply.github.com>
2023-05-01 15:44:04 -0300

    feat: add sign and logout

--- src/app/components/admin/pages/home/home.component.ts
+++ src/app/components/admin/pages/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  constructor(private readonly authService: AuthService) {}
 
+  public logout() {
+    this.authService.logout();
+  }
 }
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable, catchError, map, throwError } from 'rxjs';
 
 @Injectable({
@@ -7,14 +8,19 @@ import { Observable, catchError, map, throwError } from 'rxjs';
 })
 export class AuthService {
   private url: string = 'http://localhost:3000';
-  constructor(private readonly httpClient: HttpClient) {}
+  constructor(
+    private readonly httpClient: HttpClient,
+    private readonly router: Router
+  ) {}
 
   public sign(payload: { email: string; password: string }): Observable<any> {
     return this.httpClient
-      .post<{ email: string; password: string }>(`${this.url}/sign`, payload)
+      .post<{ token: string }>(`${this.url}/sign`, payload)
       .pipe(
         map((res) => {
-          return console.log(res);
+          localStorage.removeItem('access_token');
+          localStorage.setItem('access_token', res.token);
+          return this.router.navigate(['admin']);
         }),
         catchError((e) => {
           if (e.error.message) return throwError(() => e.error.message);
@@ -25,5 +31,10 @@ export class AuthService {
           );
         })
       );
+  }
+
+  public logout() {
+    localStorage.removeItem('access_token');
+    return this.router.navigate(['']);
   }
 }
